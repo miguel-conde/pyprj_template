af53b45189e5dbce8f29b478c40d2a0ea5a76632
miguelco2000 <miguelco2000@gmail.com>
2022-09-14 17:00:34 +0200

    setup.py: setup para paquetes de verdad

--- setup.py
+++ setup.py
@@ -4,3 +4,21 @@ setup(
     name="TstPyPaths",
     packages=["libTstPyPaths"]
 )
+
+# setup(
+#     name="pym3",
+#     version='0.1.1',
+#     description='Python for MMM projects',
+#     install_requires=[
+#         'pandas',
+#         'statsmodels',
+#         'patsy',
+#         'pickle.mixin',
+#         'matplotlib',
+#         'plotly',
+#         'sklearn',
+#         'pyomo'
+#         ],
+#     package_data={'': ['*.py']},
+#     packages=find_packages()
+# )
